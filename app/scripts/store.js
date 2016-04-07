eb822eb37068d500ad739a1722c9a7911325d009
Richard Pullinger <r_pullinger@hotmail.com>
2016-04-07 12:49:07 +0100

    Add redux thunk

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,7 +1,9 @@
-import { createStore, applyMiddlewares, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import {responsiveStoreEnhancer} from 'redux-responsive';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
 export default createStore(reducers, compose(
-    responsiveStoreEnhancer
+    responsiveStoreEnhancer,
+    applyMiddleware(thunk)
 ));
