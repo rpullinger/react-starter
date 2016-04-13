988d532376565a9689942bd3d52d497a9f3dce66
Richard Pullinger <r_pullinger@hotmail.com>
2016-04-13 16:46:38 +0100

    Add component testing using enzyme

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,35 @@ let config = {
         filename: 'bundle.js'
     },
     module: {
+        noParse: [
+            /node_modules\/sinon\//,
+        ],
         loaders: [{
             test: /\.js$/,
             exclude: /(node_modules)/,
-            loader: 'babel-loader',
+            loader: 'babel-loader'
+        }, {
+            test: /\.json$/,
+            exclude: /(node_modules)/,
+            loader: 'json-loader'
         }]
     },
     resolve: {
-        extensions: ['', '.js', '.json']
+        extensions: ['', '.js', '.json'],
+        alias: {
+            'sinon': 'sinon/pkg/sinon'
+        }
+    },
+    externals: {
+        'cheerio': 'window',
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true,
+        'react/addons': true,
     },
     devServer: {
         historyApiFallback: true
     }
 };
+
 
 module.exports = config;
